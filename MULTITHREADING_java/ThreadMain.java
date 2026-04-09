61d956c7c8793fba6cacd0753f2474d3153e2d6f
shivani.mishra152006@gmail.com <shivani.mishra152006@gmail.com>
2026-04-09 18:41:10 +0530

    initial commit

---
+++ MULTITHREADING_java/ThreadMain.java
@@ -0,0 +1,14 @@
+package MULTITHREADING_java;
+
+class userDefined extends Thread {
+    public void run() {
+        System.out.println("Thread is calling!");
+    }
+}
+
+class ThreadMain {
+    public static void main(String... aaa) {
+        userDefined ud = new userDefined();
+        ud.start(); // start() method for thread class
+    }
+}
